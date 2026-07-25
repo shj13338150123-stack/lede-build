cfdca2eb9bfe33cf4a74e1b6f13920bc07107d0c
shj13338150123-stack <shj13338150123@gmail.com>
2026-07-25 14:19:31 +0800

    Add diy-part1.sh

---
+++ diy-part1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# DIY script - Part 1: runs after feeds update, before config
+# Modify default settings here
+
+# Change default IP to 192.168.1.1
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
+
+# Add build info to banner
+echo "$(date +%Y%m%d) - LEDE X86" > package/base-files/files/etc/banner
+
+# Add custom feeds or patches here
+# echo "src-git custom https://github.com/username/custom-packages.git" >> feeds.conf.default
