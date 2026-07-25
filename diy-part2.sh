00bd43c49345ea45047942d72f461f02722d6e77
shj13338150123-stack <shj13338150123@gmail.com>
2026-07-25 14:19:33 +0800

    Add diy-part2.sh

---
+++ diy-part2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# DIY script - Part 2: runs after config is applied, before compilation
+# Modify config or add extra packages here
+
+# Enable additional packages
+# echo "CONFIG_PACKAGE_xxx=y" >> .config
+# make defconfig
+
+echo "DIY part 2 completed."
